0293cb598661001752db5c405e2d0e26237609ee
mehran9 <mgranfar@gmail.com>
2024-11-29 14:36:35 +1100

    try this

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,21 +1,57 @@
-import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate'
+// // client with accelerate
+
+// import { PrismaClient } from "@prisma/client";
+// import { withAccelerate } from '@prisma/extension-accelerate'
+
+// declare global {
+//   namespace NodeJS {
+//     interface Global {
+//       prisma: PrismaClient;
+//     }
+//   }
+// }
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
+// } else {
+//   const globalWithPrisma = global as typeof globalThis & { prisma?: PrismaClient };
+
+//   if (!globalWithPrisma.prisma) {
+//     globalWithPrisma.prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
+//   }
+
+//   prisma = globalWithPrisma.prisma;
+// }
+
+// export default prisma;
+
+
+// Normal client
+
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient;
+    }
+  }
 }
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
+  prisma = new PrismaClient()as unknown as PrismaClient;
 } else {
-  if (!global.prisma) {
-    prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
+  const globalWithPrisma = global as typeof globalThis & { prisma?: PrismaClient };
 
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient() as unknown as PrismaClient;
   }
 
-  prisma = global.prisma as PrismaClient;
+  prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
