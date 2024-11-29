d1756826ff90ffac4031ecff756f4f86c647e2e1
mehran9 <mgranfar@gmail.com>
2024-11-29 11:05:58 +1100

    changes to resolve prisma migrate issues
    updates to address migration fail issues... this has directUrl pointing to the railway postrgres db (temporarily to see if all the other changes will run properly in the production instance of adaptic-backend)... that said, we will need to swap out the direct url to point to the postgres instance on prisma.io (once they show me how to obtain it...)

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -6,11 +6,20 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
+let prisma: PrismaClient;
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient()
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
     .$extends(withAccelerate())
-    .$extends(withPulse({ apiKey: process.env.PULSE_API_KEY || '' }));
+    .$extends(withPulse({ apiKey: process.env.PULSE_API_KEY || '' })) as unknown as PrismaClient;
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+      .$extends(withAccelerate())
+      .$extends(withPulse({ apiKey: process.env.PULSE_API_KEY || '' })) as unknown as PrismaClient;
+  }
 
-global.prisma = prisma as PrismaClient;
+  prisma = global.prisma as PrismaClient;
+}
+
+export default prisma;
