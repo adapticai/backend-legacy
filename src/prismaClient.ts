5ee9896410080eba4d362e6d201c3c51a384bd11
mehran9 <mgranfar@gmail.com>
2024-11-30 22:56:16 +1100

    point to client version of prisma client (as opposed to edge version.... ugh)

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,5 +1,5 @@
 // client with accelerate
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 declare global {
