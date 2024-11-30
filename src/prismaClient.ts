1f77b0b4f61e27008e6eb21ee7be80be12100b42
mehran9 <mgranfar@gmail.com>
2024-11-30 21:59:06 +1100

    extending to create to instances of prisma client.... one for long-running (i.e. railway instance of adaptic-backend) and another called prismaServerless (used for serverless functions - i.e. usage of adaptic-backend functions within lambda functions and within adaptic-app

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,4 +1,4 @@
-// client with accelerate
+import { PrismaClient as PrismaClientServerless } from "@prisma/client/edge";
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from '@prisma/extension-accelerate'
 
@@ -6,22 +6,31 @@ declare global {
   namespace NodeJS {
     interface Global {
       prisma: PrismaClient;
+      prismaServerless: PrismaClientServerless;
     }
   }
 }
 
 let prisma: PrismaClient;
+let prismaServerless: PrismaClientServerless;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
+  prisma = new PrismaClient();
+  prismaServerless = new PrismaClientServerless().$extends(withAccelerate()) as unknown as PrismaClientServerless;
 } else {
-  const globalWithPrisma = global as typeof globalThis & { prisma?: PrismaClient };
+  const globalWithPrisma = global as typeof globalThis & { prisma?: PrismaClient; prismaServerless?: PrismaClientServerless };
 
   if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+
+  if (!globalWithPrisma.prismaServerless) {
+    globalWithPrisma.prismaServerless = new PrismaClientServerless().$extends(withAccelerate()) as unknown as PrismaClientServerless;
   }
 
   prisma = globalWithPrisma.prisma;
+  prismaServerless = globalWithPrisma.prismaServerless;
 }
 
 export default prisma;
+export { prismaServerless };
