342b376508468dd157c35ce7b0d6b4d14eb48945
mehran9 <mgranfar@gmail.com>
2024-11-30 22:40:13 +1100

    try again you son of a bitch

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,36 +1,27 @@
-import { PrismaClient as PrismaClientServerless } from "@prisma/client/edge";
-import { PrismaClient } from "@prisma/client";
+// client with accelerate
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 declare global {
   namespace NodeJS {
     interface Global {
       prisma: PrismaClient;
-      prismaServerless: PrismaClientServerless;
     }
   }
 }
 
 let prisma: PrismaClient;
-let prismaServerless: PrismaClientServerless;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-  prismaServerless = new PrismaClientServerless().$extends(withAccelerate()) as unknown as PrismaClientServerless;
+  prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
 } else {
-  const globalWithPrisma = global as typeof globalThis & { prisma?: PrismaClient; prismaServerless?: PrismaClientServerless };
+  const globalWithPrisma = global as typeof globalThis & { prisma?: PrismaClient };
 
   if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient();
-  }
-
-  if (!globalWithPrisma.prismaServerless) {
-    globalWithPrisma.prismaServerless = new PrismaClientServerless().$extends(withAccelerate()) as unknown as PrismaClientServerless;
+    globalWithPrisma.prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
   }
 
   prisma = globalWithPrisma.prisma;
-  prismaServerless = globalWithPrisma.prismaServerless;
 }
 
 export default prisma;
-export { prismaServerless };
