eea796e101e534ad67a3e751c46cfd9d99df45af
mehran9 <mgranfar@gmail.com>
2024-12-06 14:31:44 +1100

    attempting to fix db connection issues... please God please

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,20 +1,23 @@
-// client with accelerate
 import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 declare global {
   namespace NodeJS {
     interface Global {
-      prisma: PrismaClient;
+      prisma?: PrismaClient;
     }
   }
 }
 
+const isProduction = process.env.NODE_ENV === "production";
+
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
+if (isProduction) {
+  // In production, use Prisma Data Proxy by connecting via DATABASE_URL
   prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
 } else {
+  // In development, prevent multiple instances due to hot-reloading
   const globalWithPrisma = global as typeof globalThis & { prisma?: PrismaClient };
 
   if (!globalWithPrisma.prisma) {
