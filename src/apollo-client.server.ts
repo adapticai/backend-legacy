e2d6ac35ac7ae7a27a66b0e17064b9d18a42f6c4
mehran9 <mgranfar@gmail.com>
2025-02-02 21:42:47 +1100

    update to apollo-client.server.ts implementation

--- src/apollo-client.server.ts
+++ src/apollo-client.server.ts
@@ -8,28 +8,9 @@ import type {
   NormalizedCacheObject,
 } from "@apollo/client";
 
-// Use require() to load the runtime implementations.
-const pkg = require("@apollo/client");
-
-// Use a fallback check: try the named export first, then pkg.default if needed.
-const ApolloClient =
-  pkg.ApolloClient || (pkg.default && pkg.default.ApolloClient);
-const InMemoryCache =
-  pkg.InMemoryCache || (pkg.default && pkg.default.InMemoryCache);
-const HttpLink = pkg.HttpLink || (pkg.default && pkg.default.HttpLink);
-const gql = pkg.gql || (pkg.default && pkg.default.gql);
-const ApolloError =
-  pkg.ApolloError || (pkg.default && pkg.default.ApolloError);
-const split = pkg.split || (pkg.default && pkg.default.split);
-
-// Some sub‑modules require a different path.
-const contextPkg = require("@apollo/client/link/context/context.cjs");
-const setContext =
-  contextPkg.setContext || (contextPkg.default && contextPkg.default.setContext);
-
-const errorPkg = require("@apollo/client/link/error/error.cjs");
-const onError =
-  errorPkg.onError || (errorPkg.default && errorPkg.default.onError);
+import { ApolloClient, InMemoryCache, HttpLink, gql, split, ApolloError } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context/context.cjs";
+import { onError } from "@apollo/client/link/error/error.cjs";
 
 // Re‑export the runtime implementations.
 export {
